2055d98fb70122d698c14a8f4b33ea2440811348
a.mangir <a.mangir@dextechnology.com>
2020-01-15 15:59:19 +0200

    path

--- src/core/common/configureStore.ts
+++ src/core/common/configureStore.ts
@@ -7,7 +7,7 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 export const history = createBrowserHistory();
 
-const persistWhitelist: Array<keyof IAppState> = ["current"]
+const persistWhitelist: Array<keyof IAppState> = ["orderDate", "current"]
 const persistConfig = {
   key: "root",
   storage,
